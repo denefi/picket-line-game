cd72a7d6aacb6e6c466e9ce03735577025e39d89
denefi <denefi@posteo.de>
2023-03-07 09:05:44 +0100

    Feat: enemy class
    * implement enemy class
    * implement moving of enemy

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 class Game {
     constructor() {
+        this.boardElm = null;
         this.player = null;
         this.enemy = null;
         this.obstacles = [];
-        this.boardElm = document.getElementById("board");
-        this.boardElm.id = "board";
     }
     startGame() {
         this.createBasicElements();
@@ -12,23 +11,34 @@ class Game {
         this.startGameLogik();
     }
     createBasicElements() {
+        this.boardElm = document.getElementById("board");
+        this.boardElm.id = "board";
         this.player = new Player(20, [0, 0]);
         this.boardElm.appendChild(this.player.playerElm);
-        this.enemy = new Enemy();
+        this.enemy = new Enemy(20, [0, 50]);
+        this.boardElm.appendChild(this.enemy.enemyElm);
     }
     setEventListeners() {
-        document.addEventListener("keydown", event => {
-            if(event.key === "ArrowUp") {
+        document.addEventListener("keydown", (event) => {
+            if (event.key === "ArrowUp") {
                 this.player.moveUp();
-            }
-            else if (event.key === "ArrowDown") {
+            } else if (event.key === "ArrowDown") {
                 this.player.moveDown();
             }
-            
         });
     }
-    startGameLogik() {}
-
+    startGameLogik() {
+        let timeCounter = 0;
+        setInterval(() => {
+            timeCounter++;
+            if (timeCounter % 17 === 0) {
+                this.enemy.move();
+            }
+            if (timeCounter > 9007199254740990) {
+                timeCounter = 0;
+            }
+        }, 1);
+    }
 }
 
 class Player {
@@ -38,14 +48,14 @@ class Player {
         this.size = size;
         this.posX = position[0];
         this.posY = position[1];
-        this.createDiv();
+        this.createElement();
     }
-    createDiv() {
-        // Creates DOM Element and sets id, size and position on board. 
+    createElement() {
+        // Creates DOM Element and sets id, size and position on board.
         this.playerElm = document.createElement("div");
         this.playerElm.id = "player";
         this.playerElm.style.height = this.size + "%";
-        this.playerElm.style.width = this.size / 16 * 9 + "%";
+        this.playerElm.style.width = (this.size / 16) * 9 + "%";
         this.playerElm.style.bottom = this.posY + "%";
         this.playerElm.style.left = this.posX + "%";
     }
@@ -54,18 +64,52 @@ class Player {
         if (this.posY + this.size > 100) {
             this.posY = 100 - this.size;
         }
-        console.log(this.playerElm.style.bottom)
+        console.log(this.playerElm.style.bottom);
         this.playerElm.style.bottom = this.posY + "%";
     }
     moveDown() {
         this.posY -= 20;
-        if (this.posY  < 0) {
+        if (this.posY < 0) {
             this.posY = 0;
         }
         this.playerElm.style.bottom = this.posY + "%";
     }
 }
-class Enemy {}
+class Enemy {
+    constructor(size, positionArr) {
+        this.size = size;
+        this.posX = (positionArr[0] / 16) * 9;
+        this.posY = positionArr[1];
+        this.movingDirection = "up";
+        this.createElm();
+    }
+    createElm() {
+        this.enemyElm = document.createElement("div");
+        this.enemyElm.id = "enemy";
+        this.enemyElm.style.height = this.size + "%";
+        this.enemyElm.style.width = (this.size / 16) * 9 + "%";
+        this.enemyElm.style.bottom = this.posY + "%";
+        this.enemyElm.style.right = this.posX + "%";
+    }
+    move() {
+        if (this.movingDirection === "up") {
+            if (this.posY >= 100 - this.size) {
+                this.movingDirection = "down";
+                console.log("change direction");
+            } else {
+                this.posY++;
+            }
+        } else if (this.movingDirection === "down") {
+            if (this.posY <= 0) {
+                this.movingDirection = "up";
+            } else {
+                this.posY--;
+            }
+        }
+
+        this.enemyElm.style.bottom = this.posY + "%";
+    }
+}
 
 const myGame = new Game();
 myGame.startGame();
